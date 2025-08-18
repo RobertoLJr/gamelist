63fd563d188f3416f9ddafe1484c16c9e1eaf282
Roberto Junior <gsn.roberto@gmail.com>
2025-08-18 13:23:13 -0300

    feat: add GameSummaryDTO

---
+++ src/main/java/com/robertoljr/gamelist/dto/GameSummaryDTO.java
@@ -0,0 +1,42 @@
+package com.robertoljr.gamelist.dto;
+
+import com.robertoljr.gamelist.entity.Game;
+
+public class GameSummaryDTO {
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameSummaryDTO() {
+    }
+
+    public GameSummaryDTO(Game entity) {
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+}
