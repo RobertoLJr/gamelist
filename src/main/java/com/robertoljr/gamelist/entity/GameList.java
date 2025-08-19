ca023eeb24d3f5904f8f8ab297161742f13db2d4
Roberto Junior <gsn.roberto@gmail.com>
2025-08-19 16:27:43 -0300

    feat: add entity GameList POJO (no ORM)

---
+++ src/main/java/com/robertoljr/gamelist/entity/GameList.java
@@ -0,0 +1,51 @@
+package com.robertoljr.gamelist.entity;
+
+import java.util.Objects;
+
+public class GameList {
+    private Long id;
+    private String name;
+
+    public GameList() {
+    }
+
+    public GameList(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        GameList gameList = (GameList) o;
+        return Objects.equals(getId(), gameList.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "GameList{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
