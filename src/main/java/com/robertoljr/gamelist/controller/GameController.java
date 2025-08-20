77d5ee9704ac16a6f81b622d8e896b6ae2ee2cdc
Roberto Junior <gsn.roberto@gmail.com>
2025-08-19 21:15:24 -0300

    feat: implement GetMapping for findById() method on GameController

--- src/main/java/com/robertoljr/gamelist/controller/GameController.java
+++ src/main/java/com/robertoljr/gamelist/controller/GameController.java
@@ -1,10 +1,13 @@
 package com.robertoljr.gamelist.controller;
 
+import com.robertoljr.gamelist.dto.GameInfoDTO;
 import com.robertoljr.gamelist.dto.GameSummaryDTO;
 import com.robertoljr.gamelist.service.GameService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +27,10 @@ public class GameController {
     @GetMapping()
     public ResponseEntity<List<GameSummaryDTO>> findAll() {
         return  ResponseEntity.ok(gameService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<GameInfoDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(gameService.findById(id));
     }
 }
