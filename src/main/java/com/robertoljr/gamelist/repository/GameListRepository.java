0018b0a3d07d97be949a20710ad41213ba578ed6
Roberto Junior <gsn.roberto@gmail.com>
2025-08-19 21:20:10 -0300

    feat: add GameListRepository

---
+++ src/main/java/com/robertoljr/gamelist/repository/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.robertoljr.gamelist.repository;
+
+import com.robertoljr.gamelist.entity.GameList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+}
